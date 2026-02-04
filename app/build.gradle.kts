0095b4a8fa735b372500ef5255b477a5376d48a3
ChaoMixian <chaomingxun@outlook.com>
2026-02-04 21:42:49 +0800

    fix(WhileModule): 修复摘要语义问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.chaomixian.vflow"
         minSdk = 29
         targetSdk = 36
-        versionCode = 28
-        versionName = "1.4.2-pr3"
+        versionCode = 29
+        versionName = "1.4.2-pr4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
